572dd8f8e7cb4ea4da26434d145c98a987de0a7f
Miguel-Bit-Debug <migueldossantos011@gmail.com>
2021-09-21 23:24:09 -0300

    Adicionar arquivos de projeto.

---
+++ Project.Domain/Models/BaseModel.cs
@@ -0,0 +1,14 @@
+﻿using System;
+namespace Project.Domain.Models
+{
+    public abstract class BaseModel
+    {
+
+        protected BaseModel()
+        {
+            Id = Guid.NewGuid();
+        }
+
+        public Guid Id { get; set; }
+    }
+}
---
+++ Project.Domain/Models/CustomerModel.cs
@@ -0,0 +1,11 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Project.Domain.Models
+{
+    public class CustomerModel : BaseModel
+    {
+        [Required]
+        [Display(Name = "Nome")]
+        public string Name { get; set; }
+    }
+}
